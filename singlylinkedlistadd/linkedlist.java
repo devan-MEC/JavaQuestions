df0b76d9c7199f71d317bdaf26cd32e46b65ab09
Devanarayan J <devan23.5.2001@gmail.com>
2020-09-25 08:34:20 +0530

    files added

--- singlylinkedlistadd/linkedlist.java
+++ singlylinkedlistadd/linkedlist.java
@@ -1,4 +1,4 @@
-class Solution {
+class linkedlist {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead=new ListNode(0);
         ListNode p=l1,q=l2, curr=dummyHead;
